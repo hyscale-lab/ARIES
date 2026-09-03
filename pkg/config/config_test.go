3473ba0b59f7fcdacda98dc8e599f444b9bd3491
Fluenta <ananaskelly@mail.ru>
2026-09-04 00:58:23 +0500

    fix: fix num of profiles in config_test.go

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -588,7 +588,9 @@ func TestCheckedInProfilesLoad(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+
 	if len(paths) != 14 {
+
 		t.Fatalf("profiles=%v", paths)
 	}
 	for _, path := range paths {
